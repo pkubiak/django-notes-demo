6fd56b14184567d13a30d452c0c122171b8e62dc
Paweł Kubiak <pawel.kubiak.it@gmail.com>
2019-11-27 20:13:08 +0100

    Ex5. Create Topic model

--- notes/admin.py
+++ notes/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 
-from notes.models import Note
+from notes.models import Note, Topic
 
 
 class NoteAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'published_at')
 
 
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('slug', 'title', 'description', 'created', 'modified')
+    pass
+
+
 admin.site.register(Note, NoteAdmin)
+admin.site.register(Topic, TopicAdmin)
--- notes/models.py
+++ notes/models.py
@@ -2,17 +2,20 @@ import time
 from django.db import models
 from django.conf import settings
 from django.urls import reverse
+from django_extensions.db.models import TitleSlugDescriptionModel, TimeStampedModel
+
+
+class Topic(TitleSlugDescriptionModel, TimeStampedModel, models.Model):
+    def __str__(self):
+        return self.title
 
 
 class Note(models.Model):
     title = models.CharField(max_length=256)
     body = models.TextField()
     published_at = models.DateTimeField()
-
-    author = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-    )
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, null=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def get_absolute_url(self):
         return reverse('note-detail', kwargs={'pk': self.pk})
